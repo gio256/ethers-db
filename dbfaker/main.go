ca16b1189b9c6daf44449210c1412131065991d8
gio256 <102917377+gio256@users.noreply.github.com>
2022-04-14 22:27:13 -0600

    go fmt: 200 line diff. Am I a bad gopher?

--- dbfaker/main.go
+++ dbfaker/main.go
@@ -6,16 +6,16 @@ package main
 import "C"
 import "runtime/cgo"
 import (
-    "log"
-    "context"
+	"context"
+	"log"
 
-    "github.com/ledgerwatch/erigon/core/types/accounts"
-    "github.com/ledgerwatch/erigon/common"
-    "github.com/ledgerwatch/erigon/core/rawdb"
-    "github.com/ledgerwatch/erigon/core/state"
-    "github.com/ledgerwatch/erigon-lib/kv/mdbx"
-    "github.com/ledgerwatch/erigon-lib/kv"
-    "github.com/holiman/uint256"
+	"github.com/holiman/uint256"
+	"github.com/ledgerwatch/erigon-lib/kv"
+	"github.com/ledgerwatch/erigon-lib/kv/mdbx"
+	"github.com/ledgerwatch/erigon/common"
+	"github.com/ledgerwatch/erigon/core/rawdb"
+	"github.com/ledgerwatch/erigon/core/state"
+	"github.com/ledgerwatch/erigon/core/types/accounts"
 	erigonLog "github.com/ledgerwatch/log/v3"
 )
 
@@ -29,110 +29,111 @@ func main() {}
 // deleted in order for the garbage collector to clean it up (call MdbxClose).
 //export MdbxOpen
 func MdbxOpen(path string) (exit int, ptr C.uintptr_t) {
-    logger := erigonLog.New()
-    db, err := mdbx.NewMDBX(logger).Path(path).Open()
-    if err != nil {
-        log.Print(err)
-        return -1, *new(C.uintptr_t)
-    }
-    f := func() kv.RwDB { return db }
-    return 1, C.uintptr_t(cgo.NewHandle(f))
+	logger := erigonLog.New()
+	db, err := mdbx.NewMDBX(logger).Path(path).Open()
+	if err != nil {
+		log.Print(err)
+		return -1, *new(C.uintptr_t)
+	}
+	f := func() kv.RwDB { return db }
+	ptr = C.uintptr_t(cgo.NewHandle(f))
+	return 1, ptr
 }
 
 // Takes a pointer to a go function returning an mdbx instance, then closes
 // that db instance and deletes the pointer handle.
 //export MdbxClose
 func MdbxClose(ptr C.uintptr_t) {
-    handle := cgo.Handle(ptr)
-    db := handle.Value().(func() kv.RwDB)()
-    db.Close()
-    handle.Delete()
-    log.Println("Go mdbx closed\n")
+	handle := cgo.Handle(ptr)
+	db := handle.Value().(func() kv.RwDB)()
+	db.Close()
+	handle.Delete()
+	log.Println("Go mdbx closed")
 }
 
 //export PutAccount
 func PutAccount(ptr C.uintptr_t, address []byte, rlpAccount []byte, incarnation uint64) (exit int) {
 
-    db := cgo.Handle(ptr).Value().(func() kv.RwDB)()
+	db := cgo.Handle(ptr).Value().(func() kv.RwDB)()
 
-    var acct accounts.Account
-    if err := acct.DecodeForHashing(rlpAccount); err != nil {
-        log.Println(err)
-        return -1
-    }
-    acct.Incarnation = incarnation
+	var acct accounts.Account
+	if err := acct.DecodeForHashing(rlpAccount); err != nil {
+		log.Println(err)
+		return -1
+	}
+	acct.Incarnation = incarnation
 
-    ctx := context.Background()
-    tx, err := db.BeginRw(ctx)
-    if err != nil {
-        log.Println(err)
-        return -1
-    }
-    defer func() {
-        if err == nil {
-            err = tx.Commit()
-        }
-        if err != nil {
-            tx.Rollback()
-        }
-    }()
+	ctx := context.Background()
+	tx, err := db.BeginRw(ctx)
+	if err != nil {
+		log.Println(err)
+		return -1
+	}
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
+		}
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
 
-    w := state.NewPlainStateWriterNoHistory(tx)
-    err = w.UpdateAccountData(common.BytesToAddress(address), new(accounts.Account), &acct)
-    if err != nil {
-        log.Println(err)
-        return -1
-    }
+	w := state.NewPlainStateWriterNoHistory(tx)
+	err = w.UpdateAccountData(common.BytesToAddress(address), new(accounts.Account), &acct)
+	if err != nil {
+		log.Println(err)
+		return -1
+	}
 
-    return 1
+	return 1
 }
 
 //export PutStorage
 func PutStorage(ptr C.uintptr_t, addressB []byte, keyB []byte, valB []byte) (exit int) {
 
-    db := cgo.Handle(ptr).Value().(func() kv.RwDB)()
+	db := cgo.Handle(ptr).Value().(func() kv.RwDB)()
 
-    address := common.BytesToAddress(addressB)
-    key := common.BytesToHash(keyB)
-    val, overflow := uint256.FromBig(common.BytesToHash(valB).Big())
-    if overflow {
-        log.Printf("Overflowed int conversion %x\n", valB)
-        return -1
-    }
+	address := common.BytesToAddress(addressB)
+	key := common.BytesToHash(keyB)
+	val, overflow := uint256.FromBig(common.BytesToHash(valB).Big())
+	if overflow {
+		log.Printf("Overflowed int conversion %x\n", valB)
+		return -1
+	}
 
-    ctx := context.Background()
-    tx, err := db.BeginRw(ctx)
-    if err != nil {
-        log.Println(err)
-        return -1
-    }
-    defer func() {
-        if err == nil {
-            err = tx.Commit()
-        }
-        if err != nil {
-            tx.Rollback()
-        }
-    }()
+	ctx := context.Background()
+	tx, err := db.BeginRw(ctx)
+	if err != nil {
+		log.Println(err)
+		return -1
+	}
+	defer func() {
+		if err == nil {
+			err = tx.Commit()
+		}
+		if err != nil {
+			tx.Rollback()
+		}
+	}()
 
-    var acct accounts.Account
-    exists, err := rawdb.ReadAccount(tx, address, &acct)
-    if err != nil {
-        log.Println(err)
-        return -1
-    }
+	var acct accounts.Account
+	exists, err := rawdb.ReadAccount(tx, address, &acct)
+	if err != nil {
+		log.Println(err)
+		return -1
+	}
 
-    var incarnation uint64 = 0
-    if exists {
-        incarnation = acct.Incarnation
-    }
+	var incarnation uint64 = 0
+	if exists {
+		incarnation = acct.Incarnation
+	}
 
-    w := state.NewPlainStateWriterNoHistory(tx)
-    err = w.WriteAccountStorage(address, incarnation, &key, new(uint256.Int), val)
-    if err != nil {
-        log.Println(err)
-        return -1
-    }
+	w := state.NewPlainStateWriterNoHistory(tx)
+	err = w.WriteAccountStorage(address, incarnation, &key, new(uint256.Int), val)
+	if err != nil {
+		log.Println(err)
+		return -1
+	}
 
-    return 1
+	return 1
 }
